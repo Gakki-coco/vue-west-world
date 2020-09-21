5097569ba07a575b1b1ed724195ff7a5f305559b
Gakki-coco <alyciahathaways@gmail.com>
2020-09-21 23:03:09 +0800

    初始化 cell 组件

---
+++ src/components/Cell.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="cell" @click="x = true">
+    <template v-if="x">{{ text }}</template>
+    <template v-else></template>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Cell',
+  data() {
+    return {
+      x: false,
+      text: 'x'
+    }
+  }
+}
+</script>
+
+<style>
+.cell {
+  border: 1px solid black;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 80px;
+}
+</style>
